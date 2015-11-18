ea57ee9e31c9552c0d67b13dec474941eee8b7b9
Gangzhou Qi <jiangzhouq@gmail.com>
2015-11-18 11:28:20 +0800

    修改imageloader相关属性

--- app/src/main/java/qjizho/vrlauncher/Launcher.java
+++ app/src/main/java/qjizho/vrlauncher/Launcher.java
@@ -93,9 +93,8 @@ public class Launcher extends AppCompatActivity {
         showLauncher();
         config = ImageLoaderConfiguration.createDefault(this);
         options = new DisplayImageOptions.Builder()
-                .showStubImage(R.mipmap.pictures)          // 设置图片下载期间显示的图片
-                .showImageForEmptyUri(R.mipmap.pictures)  // 设置图片Uri为空或是错误的时候显示的图片
-                .showImageOnFail(R.mipmap.pictures)       // 设置图片加载或解码过程中发生错误显示的图片
+                .showImageForEmptyUri(null)  // 设置图片Uri为空或是错误的时候显示的图片
+                .showImageOnFail(null)       // 设置图片加载或解码过程中发生错误显示的图片
                 .cacheInMemory(true)                        // 设置下载的图片是否缓存在内存中
                 .cacheOnDisc(true)                          // 设置下载的图片是否缓存在SD卡中
                 .build();
